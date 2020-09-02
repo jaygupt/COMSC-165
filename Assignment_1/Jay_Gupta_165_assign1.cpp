3df43d400a6277f4328f341c3f4c3f052c788cac
jaygupt <jaygupt1973@gmail.com>
2020-09-02 09:37:59 -0700

    Create two input functions

--- Assignment_1/Jay_Gupta_165_assign1.cpp
+++ Assignment_1/Jay_Gupta_165_assign1.cpp
@@ -2,11 +2,28 @@
 #include <iostream>
 using namespace std; 
 
+// 2. function prototypes; this allows me to place my functions in any order I choose 
+int promptName(int runnerNum); 
+int promptTime(int runnerNum); 
+
 int main() {
-    // Introductory statement to user, telling them what the program will be about
+    // 2. Introductory statement to user, telling them what the program will be about.
     cout << "Enter the names of three runners and their " << 
     "finishing times. I will tell you who came in first, " <<
-    "second, and third."; 
+    "second, and third." << endl; 
+
+    promptName(0); 
+    promptTime(10); 
 
     return 0; 
+}
+
+int promptName(int runnerNum) {
+    cout << "Name of Runner " << runnerNum << ": "; 
+    return 7; 
+}
+
+int promptTime(int runnerNum) {
+    cout << "Runner " << runnerNum << "'s finishing time: "; 
+    return 3; 
 }
