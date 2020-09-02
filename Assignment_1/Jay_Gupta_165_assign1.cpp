5d34b027adab07e1c1f220c1e9e572b22e543d0a
jaygupt <jaygupt1973@gmail.com>
2020-09-02 09:20:58 -0700

    Set up C++ file. Add introductory statement.

---
+++ Assignment_1/Jay_Gupta_165_assign1.cpp
@@ -0,0 +1,12 @@
+// 1. Including a header file for input/output functions
+#include <iostream>
+using namespace std; 
+
+int main() {
+    // Introductory statement to user, telling them what the program will be about
+    cout << "Enter the names of three runners and their " << 
+    "finishing times. I will tell you who came in first, " <<
+    "second, and third."; 
+
+    return 0; 
+}
