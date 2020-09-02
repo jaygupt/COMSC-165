d43b63f1d853c25dbc539a088be46f6a8ca4a9d4
jaygupt <jaygupt1973@gmail.com>
2020-09-02 11:00:03 -0700

    Receive input from all 3 runners

--- Assignment_1/Jay_Gupta_165_assign1.cpp
+++ Assignment_1/Jay_Gupta_165_assign1.cpp
@@ -1,10 +1,11 @@
 // 1. Including a header file for input/output functions
 #include <iostream>
+#include <string>
 using namespace std; 
 
 // 2. function prototypes; this allows me to place my functions in any order I choose 
-int promptName(int runnerNum); 
-int promptTime(int runnerNum); 
+string promptName(int runnerNum);
+double promptTime(int runnerNum); 
 
 int main() {
     // 2. Introductory statement to user, telling them what the program will be about.
@@ -12,18 +13,32 @@ int main() {
     "finishing times. I will tell you who came in first, " <<
     "second, and third." << endl; 
 
-    promptName(0); 
-    promptTime(10); 
+    // Get Runner 1's name and time 
+    string runnerOneName = promptName(1); 
+    double runnerOneTime = promptTime(1);
+
+    // Get Runner 2's name and time 
+    string runnerTwoName = promptName(2); 
+    double runnerTwoTime = promptTime(2); 
+
+    // Get Runner 3's name and time 
+    string runnerThreeName = promptName(3); 
+    double runnerThreeTime = promptTime(3); 
 
     return 0; 
 }
 
-int promptName(int runnerNum) {
+string promptName(int runnerNum) { 
+    char runnerName[50]; 
     cout << "Name of Runner " << runnerNum << ": "; 
-    return 7; 
+    cin.getline(runnerName, 50); 
+    return runnerName; 
 }
 
-int promptTime(int runnerNum) {
+double promptTime(int runnerNum) {
+    double runnerTime; 
     cout << "Runner " << runnerNum << "'s finishing time: "; 
-    return 3; 
+    cin >> runnerTime;  
+    cin.ignore(256, '\n');
+    return runnerTime; 
 }
