4072437df38343dc5d84aa120bc6be31a26041cc
jaygupt <jaygupt1973@gmail.com>
2020-09-13 19:25:56 -0700

    Finish Assignment Directives

--- Assignment_2/jay_gupta_165_assign2.cpp
+++ Assignment_2/jay_gupta_165_assign2.cpp
@@ -13,62 +13,67 @@ int getComputerChoice();
 void determineOutcome(int, int);
 void displayChoice(int);
 
+// TODO: Number Comments 
+// TODO: 5 Screenshots of sample runs 
+
 int main()
 {
-	// set seed for rand() using time  
+	// 1. set seed for rand() using time  
 	// rand() should only be seeded once, which
 	// is why it is being done here, at the 
 	// start of the program
 	srand(time(0)); 
 
-	determineOutcome(1, 2);
+	int userChoice;		
+	int computerChoice;	
 
-	// My Tests:
-	// int userChoice = getUserChoice();
-	// displayChoice(1); 
+	computerChoice = getComputerChoice();
 
-	// int userChoice;		
-	// int computerChoice;	
+	userChoice = getUserChoice();
 
-	// computerChoice = getComputerChoice();
+	while (userChoice != QUIT)
+	{	
+		cout << "\nYou selected: ";
+		displayChoice(userChoice);
+		cout << "\nThe computer selected: ";
+		displayChoice(computerChoice);
 
-	// userChoice = getUserChoice();
-
-	// while (userChoice != QUIT)
-	// {
+		cout << endl << endl;
 		
-	// 	cout << "\nYou selected: ";
-	// 	displayChoice(userChoice);
-	// 	cout << "The computer selected: ";
-	// 	displayChoice(computerChoice);
-		
-	// 	determineOutcome(userChoice, computerChoice);
-	// 	cout<<endl;
+		determineOutcome(userChoice, computerChoice);
+		cout << endl;
 
-	// 	computerChoice = getComputerChoice();
-
-	// 	userChoice = getUserChoice();
-	// }
+		computerChoice = getComputerChoice();
+		userChoice = getUserChoice();
+	}
 
 	system("PAUSE");
 	return 0;
 }
 
-// no input parameters 
+// 2. no input parameters 
 // returns the computer's choice (1, 2, or 3)
 int getComputerChoice()
 {
-	// generate a random number from 1-3
+	// 3. generate a random number from 1-3
+	// rand() % 3: generates a random number
+	// from 0 to 2
 	int computerChoice = 1 + (rand() % 3); 
 	
-	return computerChoice; 
+	if (computerChoice == 1) {
+		return ROCK; 
+	} else if (computerChoice == 2) {
+		return PAPER; 
+	} else {
+		return SCISSORS; 
+	} 
 }
 
-// no input parameters
+// 4. no input parameters
 // returns the user's choice (1, 2, 3, or 4)
 int getUserChoice()
 {
-    // display the game menu to the user
+    // 5. display the game menu to the user
 	cout << endl << "Game Menu" << endl; 
 	cout << "---------" << endl; 
 	cout << "1) Rock" << endl; 
@@ -76,20 +81,29 @@ int getUserChoice()
 	cout << "3) Scissors" << endl; 
 	cout << "4) Quit" << endl << endl; 
 
-	// prompt user for choice from 1-4
+	// 6. prompt user for choice from 1-4
 	int userChoice; 
 	cout << "Enter your choice: "; 
 	cin >> userChoice; 
 
-	// while userChoice falls outside the range of 1-4, 
+	// 7. while userChoice falls outside the range of 1-4, 
 	// keep on prompting the user for a valid selection 
 	while (userChoice < 1 || userChoice > 4) {
 		cout << "Invalid selection. Enter 1, 2, 3, or 4: "; 
 		cin >> userChoice; 
 	}
 
-	// returns user's choice (1, 2, 3, or 4)
-	return userChoice; 
+	// 8. returns user's choice (1, 2, 3, or 4)
+	if (userChoice == 1) {
+		return ROCK; 
+	} else if (userChoice == 2) {
+		return PAPER; 
+	} else if (userChoice == 3) {
+		return SCISSORS; 
+	} else {
+		// userChoice == 4
+		return QUIT; 
+	}
 }
 
 // input: user's choice (1, 2, or 3) and computer's choice (1, 2, or 3)
@@ -104,19 +118,19 @@ void determineOutcome(int user, int computer)
 	// tie 
 	if (user == computer) {
 		cout << "Tie. No winner."; 
-	} else if (user == 1 && computer == 3) {
+	} else if (user == ROCK && computer == SCISSORS) {
 		// user selects rock, computer selects scissors 
 		cout << "YOU win! Rock smashes scissors."; 
-	} else if (computer == 1 && user == 3) {
+	} else if (computer == ROCK && user == SCISSORS) {
 		// same as above but user and computer are reversed
 		cout << "Computer wins! Rock smashes scissors."; 
-	} else if (user == 3 && computer == 2) {
+	} else if (user == SCISSORS && computer == PAPER) {
 		// user selects scissors, computer selects paper 
 		cout << "YOU win! Scissors cuts paper."; 
-	} else if (computer == 3 && user == 2) {
+	} else if (computer == SCISSORS && user == PAPER) {
 		// same as above but user and computer are reversed
 		cout << "Computer wins! Scissors cuts paper."; 
-	} else if (user == 2 && computer == 1) {
+	} else if (user == PAPER && computer == ROCK) {
 		// user selects paper, computer selects rock 
 		cout << "YOU win! Paper wraps rock."; 
 	} else {
