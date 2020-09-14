5a8dc6a90822e3371a3d710e938a2bc940e261ac
jaygupt <jaygupt1973@gmail.com>
2020-09-13 19:37:57 -0700

    Add additional documentation

--- Assignment_2/jay_gupta_165_assign2.cpp
+++ Assignment_2/jay_gupta_165_assign2.cpp
@@ -27,12 +27,16 @@ int main()
 	int userChoice;		
 	int computerChoice;	
 
+	// 2. computerChoice is a random number from 1-3
 	computerChoice = getComputerChoice();
 
+	// 3. user selects a number from 1-4 
 	userChoice = getUserChoice();
 
 	while (userChoice != QUIT)
 	{	
+		// 4. display each object (rock, paper, or scissors)
+		// the user and computer selected
 		cout << "\nYou selected: ";
 		displayChoice(userChoice);
 		cout << "\nThe computer selected: ";
@@ -40,9 +44,12 @@ int main()
 
 		cout << endl << endl;
 		
+		// 5. display the results of the match
 		determineOutcome(userChoice, computerChoice);
 		cout << endl;
 
+		// 6. get another number for computer in 
+		// case user wants to play again
 		computerChoice = getComputerChoice();
 		userChoice = getUserChoice();
 	}
@@ -51,11 +58,11 @@ int main()
 	return 0;
 }
 
-// 2. no input parameters 
+// 7. no input parameters 
 // returns the computer's choice (1, 2, or 3)
 int getComputerChoice()
 {
-	// 3. generate a random number from 1-3
+	// 8. generate a random number from 1-3
 	// rand() % 3: generates a random number
 	// from 0 to 2
 	int computerChoice = 1 + (rand() % 3); 
@@ -69,11 +76,11 @@ int getComputerChoice()
 	} 
 }
 
-// 4. no input parameters
+// 9. no input parameters
 // returns the user's choice (1, 2, 3, or 4)
 int getUserChoice()
 {
-    // 5. display the game menu to the user
+    // 10. display the game menu to the user
 	cout << endl << "Game Menu" << endl; 
 	cout << "---------" << endl; 
 	cout << "1) Rock" << endl; 
@@ -81,19 +88,19 @@ int getUserChoice()
 	cout << "3) Scissors" << endl; 
 	cout << "4) Quit" << endl << endl; 
 
-	// 6. prompt user for choice from 1-4
+	// 11. prompt user for choice from 1-4
 	int userChoice; 
 	cout << "Enter your choice: "; 
 	cin >> userChoice; 
 
-	// 7. while userChoice falls outside the range of 1-4, 
+	// 12. while userChoice falls outside the range of 1-4, 
 	// keep on prompting the user for a valid selection 
 	while (userChoice < 1 || userChoice > 4) {
 		cout << "Invalid selection. Enter 1, 2, 3, or 4: "; 
 		cin >> userChoice; 
 	}
 
-	// 8. returns user's choice (1, 2, 3, or 4)
+	// 13. returns user's choice (1, 2, 3, or 4)
 	if (userChoice == 1) {
 		return ROCK; 
 	} else if (userChoice == 2) {
@@ -106,11 +113,11 @@ int getUserChoice()
 	}
 }
 
-// input: user's choice (1, 2, or 3) and computer's choice (1, 2, or 3)
+// 14. input: user's choice (1, 2, or 3) and computer's choice (1, 2, or 3)
 // output: displays using 'cout' who the winner is, or if there was a tie
 void determineOutcome(int user, int computer)
 {
-	// ways to win
+	// 15. ways to win
 	// rock beats scissors (1 vs. 3)
 	// scissors beats paper (3 vs. 2)
 	// paper beats rock (2 vs. 1)
@@ -139,7 +146,7 @@ void determineOutcome(int user, int computer)
 	}
 }
 
-// input: user's or computer's choice (1, 2, or 3)
+// 16. input: user's or computer's choice (1, 2, or 3)
 // output: displays using 'cout' rock, paper, or scissors 
 // depending on the number given
 void displayChoice(int choice)
