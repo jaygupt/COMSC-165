f568e418bb31fe930bfe867ff9b90fafb9e29be8
Jay Gupta <jaygupt1973@gmail.com>
2020-09-27 15:10:01 -0700

    Add base file

---
+++ Assignment_3/165_assign3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void getJudgeData(double &);
+void calcScore(double, double, double, double, double);
+double findLowest(double, double, double, double, double);
+double findHighest(double, double, double, double, double);
+
+int main()
+{
+	double score1;	
+	double score2;	
+	double score3;	
+	double score4;	
+	double score5;	
+
+	
+	getJudgeData(score1);
+	
+	//IMPLEMENT THE REST OF THE MAIN FUNCTION
+	//IMPLEMENT THE REST OF THE MAIN FUNCTION
+	//IMPLEMENT THE REST OF THE MAIN FUNCTION
+	
+	system("PAUSE");
+    return 0;
+}
+
+// IMPLEMENT THE FOUR FUNCTIONS STARTING HERE
+// IMPLEMENT THE FOUR FUNCTIONS STARTING HERE
+// IMPLEMENT THE FOUR FUNCTIONS STARTING HERE
