613d650a60ffce9a5aa6a5fb763176c052e4fe11
jaygupt <jaygupt1973@gmail.com>
2020-09-27 15:35:22 -0700

    Add function skeletons

--- Assignment_3/165_assign3.cpp
+++ Assignment_3/165_assign3.cpp
@@ -2,30 +2,40 @@
 #include <iomanip>
 using namespace std;
 
-void getJudgeData(double &);
+void getJudgeData(double &score);
 void calcScore(double, double, double, double, double);
 double findLowest(double, double, double, double, double);
 double findHighest(double, double, double, double, double);
 
 int main()
 {
-	double score1;	
-	double score2;	
-	double score3;	
-	double score4;	
-	double score5;	
+	double score1;
+	double score2;
+	double score3;
+	double score4;
+	double score5;
 
-	
+
 	getJudgeData(score1);
-	
+
 	//IMPLEMENT THE REST OF THE MAIN FUNCTION
-	//IMPLEMENT THE REST OF THE MAIN FUNCTION
-	//IMPLEMENT THE REST OF THE MAIN FUNCTION
-	
+
 	system("PAUSE");
-    return 0;
+  return 0;
 }
 
-// IMPLEMENT THE FOUR FUNCTIONS STARTING HERE
-// IMPLEMENT THE FOUR FUNCTIONS STARTING HERE
-// IMPLEMENT THE FOUR FUNCTIONS STARTING HERE
+void getJudgeData(double &score) {
+
+}
+
+void calcScore(double, double, double, double, double) {
+
+}
+
+double findLowest(double, double, double, double, double) {
+
+}
+
+double findHighest(double, double, double, double, double) {
+
+}
