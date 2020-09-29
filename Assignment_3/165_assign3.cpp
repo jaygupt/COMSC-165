984f2289b3d88d49813f3d942f4acdc06e1bec10
jaygupt <jaygupt1973@gmail.com>
2020-09-29 11:46:31 -0700

    Add findLowest and findHighest

--- Assignment_3/165_assign3.cpp
+++ Assignment_3/165_assign3.cpp
@@ -20,6 +20,10 @@ int main()
 
 
 	getJudgeData(score1);
+	getJudgeData(score2);
+	getJudgeData(score3);
+	getJudgeData(score4);
+	getJudgeData(score5);
 
 	//IMPLEMENT THE REST OF THE MAIN FUNCTION
 
@@ -38,14 +42,40 @@ void getJudgeData(double &score) {
 	}
 }
 
-void calcScore(double, double, double, double, double) {
+void calcScore(double score1, double score2, double score3, double score4, double score5) {
 
 }
 
-double findLowest(double, double, double, double, double) {
-
+double findLowest(double score1, double score2, double score3, double score4, double score5) {
+	int lowest = score1;
+	if (lowest > score2) {
+		lowest = score2;
+	}
+	if (lowest > score3) {
+		lowest = score3;
+	}
+	if (lowest > score4) {
+		lowest = score4;
+	}
+	if (lowest > score5) {
+		lowest = score5;
+	}
+	return lowest;
 }
 
-double findHighest(double, double, double, double, double) {
-
+double findHighest(double score1, double score2, double score3, double score4, double score5) {
+	int highest = score1;
+	if (highest < score2) {
+		highest = score2;
+	}
+	if (highest < score3) {
+		highest = score3;
+	}
+	if (highest < score4) {
+		highest = score4;
+	}
+	if (highest < score5) {
+		highest = score5;
+	}
+	return highest;
 }
