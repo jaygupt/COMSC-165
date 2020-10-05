795a87b4c6f953f155fdd7ffd624cbf97beb53d2
Jay Gupta <jaygupt1973@gmail.com>
2020-10-04 20:36:49 -0700

    Create changes

---
+++ Assignment_4/Jay_Gupta_165_assign4.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+using namespace std; 
+
+// function prototypes 
+void displayIncorrectAnswers(const int arr[], int arraySize); 
+void displayInstructions(); 
+void getUserInput(char arr[], int arraySize); 
+void displayResults(const char studentAnswers[], const char answerKey[], 
+    int answerKeySize, int incorrectAnswers[]); 
+
+int main() {
+    // TODO: For now, set at 5. Need to change to 20 later.
+    const int answerKeySize = 5;
+
+    // TODO: For now, set at 5 answers. Need to fill in rest
+    char answerKey[answerKeySize] = {'A', 'D', 'B', 'B', 'C'}; 
+    char studentAnswers[answerKeySize] = {}; 
+    int incorrectAnswers[answerKeySize] = {}; 
+
+    // primary instructions to the user 
+    displayInstructions(); 
+
+    getUserInput(studentAnswers, answerKeySize); 
+
+    displayResults(studentAnswers, answerKey, answerKeySize, incorrectAnswers);  
+    return 0; 
+}
+
+void displayInstructions() {
+    cout << endl << "Please enter the student's answers for each of the questions." << endl; 
+    cout << "Press Enter after typing each answer." << endl; 
+    cout << "Please enter only an A, B, C, or D for each question." << endl << endl; 
+}
+
+void getUserInput(char arr[], int arraySize) {
+    char userInput; 
+    for (int i = 0; i < arraySize; i++) {
+        cout << "Question " << i + 1 << ": "; 
+        cin >> userInput; 
+        // input validation: if userInput is not A, B, C, or D, re-prompt the user until 
+        // they input one of the correct options. 
+        while (userInput != 'A' && userInput != 'B' && userInput != 'C' && userInput != 'D') {
+            cout << "Use only an A, B, C, or D!" << endl; 
+            cout << "Please try again: "; 
+            cin >> userInput; 
+        }
+        arr[i] = userInput;  
+    }
+}
+
+void displayResults(const char studentAnswers[], const char answerKey[], 
+    int answerKeySize, int incorrectAnswers[]) {
+    // number of correct answers 
+    int correct = 0; 
+    for (int i = 0; i < answerKeySize; i++) {
+        if (studentAnswers[i] != answerKey[i]) {
+            // if the student's answer is incorrect
+
+            // at the same place in the incorrectAnswers array,
+            // store the question number 
+            incorrectAnswers[i] = i + 1; 
+        } else {
+            // if the student's answer is correct
+            correct++; 
+        }
+    }
+
+    int incorrect = answerKeySize - correct; 
+
+    cout << "Correct Answers: " << correct << endl; 
+    cout << "Incorrect Answers: " << incorrect << endl << endl; 
+    
+    cout << "Questions that were answered incorrectly:" << endl; 
+    displayIncorrectAnswers(incorrectAnswers, answerKeySize);
+}
+
+void displayIncorrectAnswers(const int arr[], int arraySize) {
+    for (int i = 0; i < arraySize; i++) {
+        if (arr[i] != 0) {
+            cout << arr[i] << endl; 
+        } 
+    }
+    cout << endl; 
+}
