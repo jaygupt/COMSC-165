d6314b0bde4c21ddad14554faf3171e377fe20fa
Jay Gupta <jaygupt1973@gmail.com>
2020-10-10 01:54:46 -0700

    Add functionality to getAverage and getTotal

--- Assignment_5/165_assign5.cpp
+++ Assignment_5/165_assign5.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 const int COLS = 5;
 
-int getTotal(int [][COLS], int);
+int getTotal(const int array[][COLS], int rows); 
 double getAverage(int [][COLS], int);
 int getRowTotal(int [][COLS], int);
 int getColumnTotal(int [][COLS], int, int);
@@ -21,22 +21,21 @@ int main()
 					{  11,  12, 13, 14, 15 },
 					{  16,  17, 18, 19, 20 } };
    
-	
+	/*
 	cout << "The total of the array elements is "
 		 << getTotal(testArray, ROWS)
 		 << endl;
    
-
 	cout << "The average value of an element is "
 		 << getAverage(testArray, ROWS)
 		 << endl;
-
+	*/
 
 	cout << "The total of row 0 is "
 		 << getRowTotal(testArray, 0)
 		 << endl;
 
-
+	/*
 	cout << "The total of col 2 is "
 		 << getColumnTotal(testArray, 2, ROWS)
 		 << endl;
@@ -49,21 +48,52 @@ int main()
 	cout << "The lowest value in row 2 is "
 		 << getLowestInRow(testArray, 2)
 		 << endl;
+	*/
         
     system("PAUSE");
 	return 0;
 }
 
 
-int getTotal(int array[][COLS], int rows)
+int getTotal(const int array[][COLS], int rows)
 {
+	// to get total, will need to have a sum variable
+	// we will loop through rows first, and then add by array[currentrow][currentcol]
+	// columns will be the second value in index
 
+	int total = 0; 
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < COLS; j++) {
+			total += array[i][j]; 
+		}
+	}
+
+	return total; 
 }
 
 
 double getAverage(int array[][COLS], int rows)
 {
-    
+	// find the total of all of the values
+	// then, multiple cols by rows to get the total number of elements
+	// average is total divided by number of elements 
+
+	int total = 0; 
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < COLS; j++) {
+			total += array[i][j]; 
+		}
+	}
+
+	int numberOfElements = COLS * rows; 
+
+	// cast numberOfElements to a double in order to get 
+	// the result in the form of a double 
+	double average = total / (double) numberOfElements; 
+
+	return average; 
 }
 
 
