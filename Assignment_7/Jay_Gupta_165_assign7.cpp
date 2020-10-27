72047ed3db57ee57ca39a9580ba29e4aa754ac30
Jay Gupta <jaygupt1973@gmail.com>
2020-10-27 16:39:36 -0700

    Add selectionSort function

--- Assignment_7/Jay_Gupta_165_assign7.cpp
+++ Assignment_7/Jay_Gupta_165_assign7.cpp
@@ -12,58 +12,49 @@ void displayResult(const string&, const string&, bool);
 void writeToFile(const string&, const vector<string>&);
 void reverseVector(vector<string>&);
 
-int main()
-{
+int main() {
    string boyName, girlName;   
    bool boyNameFound, girlNameFound;  
 
+   // vector<string> boyNames(getVector("BoyNames.txt"));
+   // vector<string> girlNames(getVector("GirlNames.txt"));
    
-   vector<string> boyNames(getVector("BoyNames.txt"));
-   vector<string> girlNames(getVector("GirlNames.txt"));
+   // boyName = getName("boy's");   
+   // girlName = getName("girl's");
    
+   // selectionSort(boyNames);  
+   // selectionSort(girlNames);
    
-   boyName = getName("boy's");   
-   girlName = getName("girl's");
+   // boyNameFound = search(boyName, boyNames); 
+   // girlNameFound = search(girlName, girlNames);
    
-   selectionSort(boyNames);  
-   selectionSort(girlNames);
+   // displayResult("boy's", boyName, boyNameFound);    
+   // displayResult("girl's", girlName, girlNameFound);
    
-   boyNameFound = search(boyName, boyNames); 
-   girlNameFound = search(girlName, girlNames);
+   // writeToFile("Boynames_asc.txt", boyNames); 
+   // writeToFile("Girlnames_asc.txt", girlNames);
    
-   displayResult("boy's", boyName, boyNameFound);    
-   displayResult("girl's", girlName, girlNameFound);
-   
-   writeToFile("Boynames_asc.txt", boyNames); 
-   writeToFile("Girlnames_asc.txt", girlNames);
-   
-   reverseVector(boyNames); 
-   reverseVector(girlNames);
+   // reverseVector(boyNames); 
+   // reverseVector(girlNames);
 
-   writeToFile("Boynames_desc.txt", boyNames); 
-   writeToFile("Girlnames_desc.txt", girlNames);
+   // writeToFile("Boynames_desc.txt", boyNames); 
+   // writeToFile("Girlnames_desc.txt", girlNames);
    
-   cout<<endl;
+   // cout << endl;
    
    system("PAUSE");
    return 0;
 }
 
-
-void selectionSort(vector<string> &arr)
-{
-	
+void selectionSort(vector<string> &arr) {
 	int startScan, minIndex;
 	string minValue;
 
-   for (startScan = 0; startScan < (arr.size() - 1); startScan++)
-   {
+   for (startScan = 0; startScan < (arr.size() - 1); startScan++) {
       minIndex = startScan;
       minValue = arr[startScan];
-      for(int index = startScan + 1; index < arr.size(); index++)
-      {
-         if (arr[index] < minValue)
-         {
+      for(int index = startScan + 1; index < arr.size(); index++) {
+         if (arr[index] < minValue) {
             minValue = arr[index];
             minIndex = index;
          }
@@ -71,7 +62,36 @@ void selectionSort(vector<string> &arr)
       arr[minIndex] = arr[startScan];
       arr[startScan] = minValue;
    }
-	
-	
 }
 
+vector<string> getVector(const string& fileName) {
+   // declare the ifstream object; reading from fileName
+   ifstream theFile(fileName); 
+
+   // declare vector to be used 
+   vector<string> theVector; 
+
+   // each data string from the file will be stored in name 
+   string name; 
+
+   // while the program hasn't reached the end of the file
+   while (theFile >> name) {
+      cout << name << endl; 
+      theVector.push_back(name); 
+   }
+
+   // close the file to utilize processing power 
+   theFile.close(); 
+
+   return theVector; 
+}
+
+string getName(const string& gender) {
+   // name to be returned
+   string name = "Placeholder"; 
+
+   cout << "Enter a " << gender << " name, or N if you do not wish "
+   << "to enter a " << gender << " name"; 
+
+   return name; 
+}
