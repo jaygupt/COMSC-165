6dd39bb0468339c61fb44828bb6d0545ef4c7d5b
Jay Gupta <jaygupt1973@gmail.com>
2020-10-30 09:23:04 -0700

    Add 10 numbered comments

--- Assignment_9/Jay_Gupta_165_assign9.cpp
+++ Assignment_9/Jay_Gupta_165_assign9.cpp
@@ -11,10 +11,10 @@ int main() {
 	int test[SIZE] = {9, 1, 7, 2, 12, 3, 9, 5};
 	int mode;
 
-	// get the mode using the test array and size constant 
+	// 1. Get the mode using the test array and size constant 
 	mode = getMode(test, SIZE);
 
-	// if mode is set to -1, the array has no mode. else, 
+	// 2. if mode is set to -1, the array has no mode. else, 
 	// there was a mode  
 	if (mode == -1)
 		cout << "\nThe test set has no mode.\n\n";
@@ -28,20 +28,20 @@ int main() {
 
 // array passed by pointer
 int getMode(int *array, int size) {
-	// stores how many times the corresponding value  
+	// 3. frequencies stores how many times the corresponding value  
 	// occurs in test array 
 	// for instance, if test[1] = 3, frequencies[1] stores how many times
 	// 3 occurs in the test array 
-	int* frequencies = makeArray(size); // intially points at first element in array 
+	int* frequencies = makeArray(size); // 4. intially points at first element in array 
 
 	// populate the frequencies array 
 	for (int i = 0; i < size; i++) {
-		// stores the current value from "array", corresponding to index i
+		// 5. stores the current value from "array", corresponding to index i
 		int currentValue = *(array + i); 
 
 		int occurrences = 0; 
 		
-		// loop through entire array to find how many times currentValue
+		// 6. loop through entire array to find how many times currentValue
 		// appears in test; increment occurrences variable accordingly
 		for (int j = 0; j < size; j++) {
 			if (currentValue == *(array + j)) {
@@ -49,16 +49,21 @@ int getMode(int *array, int size) {
 			}
 		}
 
-		// store value of occurrences at index i of frequencies. 
+		// 7. store value of occurrences at index i of frequencies. 
 		*(frequencies + i) = occurrences; 
 	}
 
-	// we have our frequencies 
-	// the highest frequency corresponds to mode in array 
+	// 8. we have our frequencies; the highest frequency 
+	// corresponds to mode in array 
 
+	// 9. Set highestFrequency to 1 initially; if no mode, 
+	// this will be determining mode as -1
 	int highestFrequency = 1; 
 	int highestFrequencyIndex = 0; 
 
+	// 10. go through the whole frequencies array; 
+	// find the largest frequency, and its corresponding index. 
+	// this index will be used to find mode. 
 	for (int i = 0; i < size; i++) {
 		if (*(frequencies + i) > highestFrequency) {
 			highestFrequency = *(frequencies + i); 
@@ -68,6 +73,8 @@ int getMode(int *array, int size) {
 
 	int mode; 
 
+	// 11. if no mode (highestFrequency is 1) return -1; else, 
+	// return the mode
 	if (highestFrequency == 1) {
 		mode = -1; 
 	} else {
