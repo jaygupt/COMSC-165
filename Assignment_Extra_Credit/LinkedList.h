2d2ce66a96109e236f7fea863d05fbfec151f9d7
Jay Gupta <jaygupt1973@gmail.com>
2020-12-06 12:57:01 -0800

    Add starting code to h file

---
+++ Assignment_Extra_Credit/LinkedList.h
@@ -0,0 +1,9 @@
+class LinkedList {
+    private: 
+        // class member variables 
+        int value; 
+        LinkedList* next; 
+        LinkedList* prev; 
+    public: 
+        // function prototypes go here 
+}; 
